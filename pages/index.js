109fe7f88c7d4a3db02f0af398c60eeb5756ddb5
Obah Sylva <sylvan452@gmail.com>
2023-06-24 22:17:54 -0400

    merge features

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import { Main } from 'next/document';
@@ -19,22 +19,50 @@ export default function Home() {
     });
   }, []);
 
+  const [from, setFrom] = useState('');
+  const [destination, setDestination] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const handleInputChange = (e, setter) => {
+    setter(e.target.value);
+  };
+
   return (
     <main>
       <Wrapper>
         <MapContainer id='MapContainer'></MapContainer>
-        <ActionItems>Start</ActionItems>
+        <LogoContainer>
+          <Image src="/images/getcement_logo.png" alt="getcement_logo" width={100} height={40} />
+        </LogoContainer>
+        <InputSection>
+          <Input placeholder="From" value={from} onChange={(e) => handleInputChange(e, setFrom)} />
+          <Input placeholder="Destination" value={destination} onChange={(e) => handleInputChange(e, setDestination)} />
+          <Input placeholder="Quantity" value={quantity} onChange={(e) => handleInputChange(e, setQuantity)} />
+          <Input placeholder="Amount" value={amount} onChange={(e) => handleInputChange(e, setAmount)} />
+        </InputSection>
+        <BookNowButton>Book Now</BookNowButton>
       </Wrapper>
     </main>
   );
 }
 
 const Wrapper = tw.div`
-flex flex-col bg-red-300 h-screen
+flex flex-col bg-white-90000 h-screen
 `;
 const MapContainer = tw.div`
 bg-red-500 flex-1
 `;
-const ActionItems = tw.div`
-bg-green-300 flex-1
+const LogoContainer = tw.div`
+  flex justify-left items-center py-2 px-2
+`;
+const InputSection = tw.div`
+  flex flex-col items-center
+`;
+
+const Input = tw.input`
+  mt-2 p-2 border border-gray-300 rounded
+`;
+const BookNowButton = tw.button`
+  mt-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 mx-2 my-2
 `;
