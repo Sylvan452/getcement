2368fdfc80a46e9cd477c56bba99e49d10177d74
Obah Sylva <sylvan452@gmail.com>
2023-07-04 10:38:57 -0400

    redirect

--- pages/confirm.js
+++ pages/confirm.js
@@ -9,15 +9,15 @@ const Confirm = () => {
     const router = useRouter();
     const { pickup, dropoff } = router.query;
 
-    console.log("pickup", pickup);
-    console.log("dropoff", dropoff);
+    console.log('pickup', pickup);
+    console.log('dropoff', dropoff);
 
-    const [pickupCoordinates, setPickupCoordinates] = useState();
-    const [dropoffCoordinates, setDropoffCoordinates] = useState();
+    const [pickupCoordinates, setPickupCoordinates] = useState(null);
+    const [dropoffCoordinates, setDropoffCoordinates] = useState(null);
 
-    const fetchPickupCoordinates = (pickup) => {
+    const fetchCoordinates = (location, setter) => {
         fetch(
-            `https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` +
+            `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?` +
             new URLSearchParams({
                 access_token:
                     'pk.eyJ1Ijoic3lsdmFuNDUyIiwiYSI6ImNsajQ4ZmszNTFuMGszbG9kancyMnZ4eGIifQ.kV1jHlVekAmurJt91LCpUw',
@@ -26,31 +26,26 @@ const Confirm = () => {
         )
             .then((response) => response.json())
             .then((data) => {
-                setPickupCoordinates(data.features[0].center);
-            });
-    };
-
-    const fetchDropoffCoordinates = (dropoff) => {
-        fetch(
-            `https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?` +
-            new URLSearchParams({
-                access_token:
-                    'pk.eyJ1Ijoic3lsdmFuNDUyIiwiYSI6ImNsajQ4ZmszNTFuMGszbG9kancyMnZ4eGIifQ.kV1jHlVekAmurJt91LCpUw',
-                limit: 1,
+                const coordinates = data.features[0]?.center;
+                if (coordinates) {
+                    setter(coordinates);
+                }
             })
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                setDropoffCoordinates(data.features[0].center);
+            .catch((error) => {
+                console.error('Error fetching coordinates:', error);
             });
     };
 
     useEffect(() => {
-        fetchPickupCoordinates(pickup);
-        fetchDropoffCoordinates(dropoff);
+        if (pickup) {
+            fetchCoordinates(pickup, setPickupCoordinates);
+        }
+        if (dropoff) {
+            fetchCoordinates(dropoff, setDropoffCoordinates);
+        }
     }, [pickup, dropoff]);
 
-    const Backbutton = () => (
+    const BackButton = () => (
         <Link href="/">
             <Back>Back</Back>
         </Link>
@@ -58,7 +53,7 @@ const Confirm = () => {
 
     return (
         <Wrapper>
-            <Backbutton />
+            <BackButton />
             <Map
                 pickupCoordinates={pickupCoordinates}
                 dropoffCoordinates={dropoffCoordinates}
@@ -73,10 +68,6 @@ const Confirm = () => {
 
 export default Confirm;
 
-const Delivery = tw.div`
-  flex-1 flex flex-col
-`;
-
 const Wrapper = tw.div`
   flex flex-col bg-white h-screen
 `;
@@ -84,9 +75,11 @@ const Wrapper = tw.div`
 const ConfirmContainer = tw.div`
   flex-1 flex flex-col
 `;
+
 const ConfirmButton = tw.div`
   my-4 mx-4 p-2 bg-blue-500 text-white text-center border-t-4 rounded hover:bg-blue-600 mx-2 my-2 text-2xl cursor-pointer w-full
 `;
+
 const Back = tw.div`
   bg-white text-xl absolute top-4 z-10 px-3 shadow-md cursor-pointer
 `;
